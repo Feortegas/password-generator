20d11aae4bd441ad6b31d32e6451c11ddfbb8ba1
Fernando Ortega <feortegas@gmail.com>
2021-10-26 00:09:43 -0500

    added comments to code

--- app/js/script.js
+++ app/js/script.js
@@ -2,40 +2,49 @@
 generatePassword = function() {
   var password = "";
   
-  // ask for user input
+  // ask for user inputs
   passwordCriteria.length = getPasswordLength();
   passwordCriteria.hasNumbers = getHasNumbers();
   passwordCriteria.hasLowercase = getHasLowercase();
   passwordCriteria.hasUppercase = getHasUppercase();
   passwordCriteria.hasSpecialChars = getHasSpecialChars();
 
+  // reset the str to empty just in case
   var str = "";
 
+  // if true, add all possible number characters
   if (passwordCriteria.hasNumbers) {
     str = str + "0123456789";
   }
 
+  // if true, add all possible lowercase characters
   if (passwordCriteria.hasLowercase) {
     str = str + "abcdefghijklmnopqrstuvwxyz";
   }
 
+  // if true, add all possible uppercase characters
   if (passwordCriteria.hasUppercase) {
     str = str + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
+  // if true, add all possible special characters
   if (passwordCriteria.hasSpecialChars) {
     str = str + "!@#$%^&*?";
   }
 
+  // loops as many times as the defined password length
+  // select the ramdom character and adds it to the password variable
   for (let index = 0; index < passwordCriteria.length; index++) {
     var char = Math.floor(Math.random() * str.length + 1);
 
     password = password + str.charAt(char);
   }
+  // returns the password value to be writen in the textarea.
   return password;
   
 }
 
+// ask for user input - password length
 var getPasswordLength = function() {
   var length = 0;
   while (length < 8 || length > 128) {
@@ -44,30 +53,35 @@ var getPasswordLength = function() {
   return length;
 };
 
+// ask for user input - if it has numbers on it
 var getHasNumbers = function() {
   var hasNumbers = false;
   hasNumbers = confirm("Does the Password include Numbers?");
   return hasNumbers;
 };
 
+// ask for user input - if it has lowercase characters on it
 var getHasLowercase = function() {
   var hasLowercase = false;
   hasLowercase = confirm("Does the Password include Lowercase Letters?");
   return hasLowercase;
 };
 
+// ask for user input - if it has uppercase characters on it
 var getHasUppercase = function() {
   var hasUppercase = false;
   hasUppercase = confirm("Does the Password include Uppercase Letters?");
   return hasUppercase;
 };
 
+// ask for user input - if it has special characters on it
 var getHasSpecialChars = function() {
   var hasSpecialChars = false;
   hasSpecialChars = confirm("Does the Password include Special Characteres?");
   return hasSpecialChars;
 };
 
+// global variables
 var passwordCriteria = {
   length : "",
   hasNumbers: "",
