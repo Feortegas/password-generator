df6610d3c539ad71f627c5b9c680afbeb69bd71e
Fernando Ortega <feortegas@gmail.com>
2021-10-26 00:22:30 -0500

    added check to make sure at least one character type is selected

--- app/js/script.js
+++ app/js/script.js
@@ -11,33 +11,44 @@ generatePassword = function() {
 
   // reset the str to empty just in case
   var str = "";
+  var atLeastOneSelection = 0;
 
   // if true, add all possible number characters
   if (passwordCriteria.hasNumbers) {
     str = str + "0123456789";
+    atLeastOneSelection++;
   }
 
   // if true, add all possible lowercase characters
   if (passwordCriteria.hasLowercase) {
     str = str + "abcdefghijklmnopqrstuvwxyz";
+    atLeastOneSelection++;
   }
 
   // if true, add all possible uppercase characters
   if (passwordCriteria.hasUppercase) {
     str = str + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    atLeastOneSelection++;
   }
 
   // if true, add all possible special characters
   if (passwordCriteria.hasSpecialChars) {
     str = str + "!@#$%^&*?";
+    atLeastOneSelection++;
   }
 
-  // loops as many times as the defined password length
-  // select the ramdom character and adds it to the password variable
-  for (let index = 0; index < passwordCriteria.length; index++) {
-    var char = Math.floor(Math.random() * str.length + 1);
-
-    password = password + str.charAt(char);
+  // at least one type of character must be selected
+  if (atLeastOneSelection > 0) {
+    // loops as many times as the defined password length
+    // select the ramdom character and adds it to the password variable
+    for (let index = 0; index < passwordCriteria.length; index++) {
+      var char = Math.floor(Math.random() * str.length + 1);
+  
+      password = password + str.charAt(char);
+    }
+  }
+  else {
+    alert("At least one Character type must be selected. Try again.");
   }
   // returns the password value to be writen in the textarea.
   return password;
@@ -98,7 +109,13 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // if password is empty, then run the generator again.
+  if (password == "") {
+    password = generatePassword();
+  }
+  else {
+    passwordText.value = password;
+  }
 
 }
 
