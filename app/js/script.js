8561f8c6dd55fcddcafa272f082b803e9cc1a888
Fernando Ortega <feortegas@gmail.com>
2021-10-25 23:07:04 -0500

    add user input for password length, check that lenght is in range

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+generatePassword = function() {
+  
+  // ask user for passwor lenght - it must be between 8 and 128 chars.
+  var passwordLength = window.prompt("What's the Passowrd Lenght - enter a number between 8 and 128.");
+  
+  // check password length
+  while (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password Length must be between 8 and 128, try again.");
+    passwordLength = window.prompt("What's the Passowrd Lenght - enter a number between 8 and 128.");
+  }
+  
+}
 
+//password character type confirm input
+// var passwordWithNumbers = window.confirm("Does the Password include Numbers?");
+// var passwordWithLowercaseLetters = window.confirm("Does the Password include Lowercase Letters?");
+// var passwordWithUppercaseLetters = window.confirm("Does the Password include Uppercase Letters?");
+// var passwordWithSpecialChars = window.confirm("Does the Password include Special Characteres?");
+// console.log("Numbers: "+ passwordWithNumbers + " Uppercase: " + passwordWithUppercaseLetters + " Lowercase " + passwordWithLowercaseLetters + " Special Chars: " + passwordWithSpecialChars);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
